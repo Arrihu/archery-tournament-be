d1c1148c56a7cb47bbee86555ced9455757c722b
ahadsenja <hamdanilm96@gmail.com>
2019-03-20 16:07:27 +0800

    feat: add rule_competition url route

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const userRoute = require('./user/router')
 const roleRoute = require('./role/router')
 const bowRoute = require('./bow/router')
 const arrowRoute = require('./arrow/router')
+const ruleRoute = require('./rule_competition/router')
 
 const app = express();
 const port = 3333
@@ -16,6 +17,7 @@ app.use('/user', userRoute)
 app.use('/role', roleRoute)
 app.use('/bow', bowRoute)
 app.use('/arrow', arrowRoute)
+app.use('/rule', ruleRoute)
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}!`)
