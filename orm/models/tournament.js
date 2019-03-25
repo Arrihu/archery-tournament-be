77713067ea3b78ffdaaa17e973805a4a5ad038aa
ahadsenja <hamdanilm96@gmail.com>
2019-03-25 14:25:39 +0800

    feat: add tournament models

---
+++ orm/models/tournament.js
@@ -0,0 +1,20 @@
+const Sequelize = require('sequelize')
+const sqlConn = require('../connection')
+const Category = require('./category')
+const EventOrganizer = require('./event_organizer')
+const Archer = require('./archer')
+
+
+const Tournament = sqlConn.define('tournament', {
+    name: Sequelize.STRING,
+    address: Sequelize.STRING,
+    start_date: Sequelize.DATE,
+    end_date: Sequelize.DATE
+})
+
+Tournament.hasMany(Category)
+Tournament.hasMany(EventOrganizer)
+Tournament.hasMany(Archer)
+Tournament.sync({force: true});
+
+module.exports = Tournament;
