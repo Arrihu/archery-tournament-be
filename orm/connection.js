cfb07a2dee718295799c8002420aa68116d80764
ahadsenja <hamdanilm96@gmail.com>
2019-03-25 14:26:38 +0800

    feat: add all table relation

--- orm/connection.js
+++ orm/connection.js
@@ -18,11 +18,69 @@ var sequelize = new Sequelize({
 });
 
 //Uncomment this ONLY WHEN you want to rebuild your databse
-// sequelize.sync({ force: true })
-//   .then(function(err) {
-//     console.log('The table was successfully created!');
-//   }, function (err) {
-//     console.log('An error occurred while creating the table:', err);
-//   });
+sequelize.sync({ force: true })
+  .then(function(err) {
+    console.log('The table was successfully created!');
+  }, function (err) {
+    console.log('An error occurred while creating the table:', err);
+  });
 
 module.exports = sequelize;
+// ======================================================
+
+
+const Archer = require('./models/archer')
+const Arrow = require('./models/arrow')
+const Bow = require('./models/bow')
+const Category = require('./models/category')
+const Club = require('./models/club')
+const EventOrganizer = require('./models/event_organizer')
+const Role = require('./models/role')
+const Score = require('./models/score')
+const Target = require('./models/target')
+const Tournament = require('./models/tournament')
+const User = require('./models/user') 
+
+
+User.belongsToMany(EventOrganizer, {
+  through: 'UserEventOrganizer',
+  foreignKey: 'userId'
+})
+
+EventOrganizer.belongsToMany(User, {
+  through: 'UserEventOrganizer',
+  foreignKey: 'eventOrganizerId'
+})
+
+
+Category.belongsToMany(Arrow, {
+  through: 'CategoryArrow',
+  foreignKey: 'categoryId'
+})
+
+Arrow.belongsToMany(Category, {
+  through: 'CategoryArrow',
+  foreignKey: 'arrowId'
+})
+
+
+Category.belongsToMany(Bow, {
+  through: 'CategoryBow',
+  foreignKey: 'categoryId'
+})
+
+Bow.belongsToMany(Category, {
+  through: 'CategoryBow',
+  foreignKey: 'bowId'
+})
+
+
+User.hasOne(Role)
+Club.hasMany(Archer)
+Archer.hasMany(Score)
+Category.hasMany(Score)
+Target.hasMany(Category)
+EventOrganizer.hasMany(Score)
+Tournament.hasMany(Archer)
+Tournament.hasMany(EventOrganizer)
+Tournament.hasMany(Category)
